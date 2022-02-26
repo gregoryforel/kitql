79451766ccdffd3e0ed5c1445920d0b9927568d5
Gregory Forel <gregory.forel@gmail.com>
2022-02-26 21:19:56 +0100

    improved

--- src/lib/ui/resume/resume.ts
+++ src/lib/ui/resume/resume.ts
@@ -3,7 +3,7 @@ import resume from '../../../resume.json'
 import theme from '../../../theme.json'
 import type { ContainerType } from './components/container.types'
 
-const loop = (branch: ContainerType, index = 0) => {
+const loop = ({ branch, index }: { branch: ContainerType; index: number }) => {
 	const resumeKey = branch?.resumeKey
 	const tag = branch?.tag
 	const containers = branch?.containers
@@ -17,42 +17,45 @@ const loop = (branch: ContainerType, index = 0) => {
 	const value = resumeKey?.replace('{{index}}', index.toString())
 		? JSONPath({ path: resumeKey?.replace('{{index}}', index.toString()), json: resume })[0]
 		: null
-	const isValueArray = Array.isArray(value)
 
 	if (isChildValueArray) {
 		const list = JSONPath({ path: childValue, json: resume })[0]
 		return {
-			// value: value || child?.resumeKey?.replace('{{index}}', index.toString()),
 			resumeKey,
 			class: branch?.class,
 			tag,
 			containers: list.map((c, i) => {
 				return loop({
-					resumeKey: child?.resumeKey?.replace('{{index}}', i.toString()),
-					tag: child?.tag,
-					class: child?.class,
-					containers: child?.containers,
+					branch: {
+						resumeKey: child?.resumeKey?.replace('{{index}}', i.toString()),
+						tag: child?.tag,
+						class: child?.class,
+						containers: child?.containers,
+					},
+					index: i,
 				})
 			}),
 		}
 	} else {
 		return {
-			value: isValueArray ? null : value || resumeKey?.replace('{{index}}', index.toString()),
+			value: Array.isArray(value)
+				? null
+				: value || resumeKey?.replace('{{index}}', index.toString()),
 			class: branch?.class,
 			tag,
 			containers: containers?.map((c, i) =>
-				loop(
-					{
+				loop({
+					branch: {
 						tag: c?.tag,
 						class: c?.class,
 						containers: c?.containers,
 						resumeKey: c?.resumeKey?.replace('{{index}}', i.toString()),
 					},
-					i
-				)
+					index: i,
+				})
 			),
 		}
 	}
 }
 
-export const themedResume = loop(theme as ContainerType)
+export const themedResume = loop({ branch: theme as ContainerType, index: 0 })
