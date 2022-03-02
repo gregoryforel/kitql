c5f29e71afc0cbc82bba12107827b86fae2d934d
Gregory Forel <gregory.forel@gmail.com>
2022-03-02 15:41:02 +0100

    created file structure for stores

--- src/lib/ui/layout/sider/sider.svelte
+++ src/lib/ui/layout/sider/sider.svelte
@@ -1,1 +1,15 @@
-<sider class="[grid-area:sider] bg-slate-400 print:hidden">Sider</sider>
+<script lang="ts">
+	import cc from 'classcat'
+</script>
+
+<sider
+	class={cc({
+		'app-sider': true,
+		'[grid-area:sider] print:hidden': true,
+		'bg-slate-400 p-8': true,
+	})}
+>
+	<button on:click={null} class="bg-white rounded text-base text-slate-900 px-2 py-1"
+		>Toggle</button
+	>
+</sider>
--- src/lib/ui/resume/resume.svelte
+++ src/lib/ui/resume/resume.svelte
@@ -27,68 +27,24 @@
 	import { buildResumeWithTheme } from './resume'
 
 	let pageInner
-	// define what element should be observed by the observer
-	// and what types of mutations trigger the callback
-
 	let pageSize: 'A4' | 'US Letter' = 'A4'
-
 	let pageWidth = pageSize === 'A4' ? '210mm' : '216mm'
 	let pageHeight = pageSize === 'A4' ? '297mm' : '279mm'
 
 	onMount(() => {
-		console.log('pageInner width', pageInner.clientWidth)
-		console.log('pageInner scrollWidth', pageInner.scrollWidth)
-		// measure()
-		// let observer = new MutationObserver(function (mutations, observer) {
-		// 	// fired when a mutation occurs
-		// 	// console.log('obs', mutations, observer)
-		// 	// console.log('obs', mutations[0].target.getBoundingClientRect().bottom)
-		// 	// ...
-		// 	mutations.forEach(function (mutation) {
-		// 		// console.log(mutation)
-		// 		// console.log(mutation.target.parentNode.getBoundingClientRect().bottom)
-		// 	})
-		// })
-		// observer.observe(document, {
-		// 	attributes: true,
-		// 	characterData: true,
-		// 	childList: true,
-		// 	subtree: true,
-		// 	attributeOldValue: true,
-		// 	characterDataOldValue: true,
-		// })
+		calculatePagination()
 	})
 
-	// function measure() {
-	// 	let box = document.querySelectorAll('h2')
-	// 	// let width = box.clientWidth
-	// 	// let height = box.clientHeight
-	// 	// console.log(box[0].getBoundingClientRect().top)
-	// 	b = box[0].getBoundingClientRect()
-	// }
+	function calculatePagination() {
+		console.log('pageInner width', pageInner.clientWidth)
+		console.log('pageInner scrollWidth', pageInner.scrollWidth)
+	}
 
-	let transfer = 0
+	export let transfer = 0
 	function toggle() {
 		transfer = transfer === 0 ? -4210 : 0
 	}
-	// let resumes
-	// ;(async () => {
-	// 	try {
-	// 		resumes = await nhost.graphql.request(
-	// 			`
-	//             {
-	//                 resumes_by_pk(id: "fa69f955-ceab-4072-9e75-d25a07f27312") {
-	//                     __typename
-	//                     id
-	//                     data
-	//                 }
-	//             }
-	//         `
-	// 		)
-	// 	} catch (error) {
-	// 		console.log('error', error)
-	// 	}
-	// })()
+
 	const themedResume = buildResumeWithTheme({ resume, theme })
 </script>
 
@@ -97,7 +53,6 @@
 </svelte:head>
 
 <div class="flex mx-auto justify-center p-8 flex-col w-fit gap-8">
-	<button on:click={toggle} class="bg-white rounded text-base text-gray-900">Toggle</button>
 	<div class="page bg-white shadow-2xl print:shadow-none">
 		<div
 			class={cc({
