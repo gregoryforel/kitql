bd93b730adec5b469cfe5ae3a75ea31a15bce6c0
Gregory Forel <gregory.forel@gmail.com>
2022-04-01 12:16:40 +0200

    playwright works

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -5,3 +5,10 @@ test('basic test', async ({ page }) => {
 	const title = page.locator('.navbar__inner .navbar__title')
 	await expect(title).toHaveText('Playwright')
 })
+
+// import { expect, test } from '@playwright/test';
+
+// test('about page has expected h1', async ({ page }) => {
+// 	await page.goto('/about');
+// 	expect(await page.textContent('h1')).toBe('About this app');
+// });
