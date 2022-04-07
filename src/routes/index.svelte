be25af14e9b94617b2fc9422f0c35982f0e97c6d
Gregory Forel <gregory.forel@gmail.com>
2022-04-07 09:05:23 +0200

    done

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
 <script context="module" lang="ts">
 	import { browser, dev } from '$app/env'
-	import { KQL_GetResumeById } from '$lib/data-access/graphql/_kitql/graphqlStores'
+	import {
+		KQL_GetResumeById,
+		KQL_UpdateResumeData,
+	} from '$lib/data-access/graphql/_kitql/graphqlStores'
 
-	// import { nhost } from '$lib/util/nhost'
 	// we don't need any JS on this page, though we'll load
 	// it in dev so that we get hot module replacement...
 	export const hydrate = dev
@@ -13,6 +15,7 @@
 	// since there's no dynamic data here, we can prerender
 	// it so that it gets served as a static asset in prod
 	export const prerender = true
+
 	export async function load({ fetch }) {
 		const id = 'fa69f955-ceab-4072-9e75-d25a07f27312'
 
@@ -25,14 +28,21 @@
 </script>
 
 <script lang="ts">
-	import { KitQLInfo } from '@kitql/all-in'
+	// import { KitQLInfo } from '@kitql/all-in'
 	import Resume from '$lib/ui/resume/resume.svelte'
+
+	const fill = () =>
+		KQL_UpdateResumeData.mutate({
+			fetch,
+			variables: { id: 'fa69f955-ceab-4072-9e75-d25a07f27312', data: null },
+		})
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
+<button on:click={fill}>FILL</button>
 <Resume resume={$KQL_GetResumeById.data.resumes_by_pk.data} />
 
 <style>
